3360aeef8e5b39ce6c7b3c5a4b90e5d8997ac0b0
anndreiboicu <58210864+anndreiboicu@users.noreply.github.com>
2022-08-22 19:25:57 +0300

    Lab - Week 4 added
    Add Lab for Week 4 of Tools for Data Science course

---
+++ Tools_for_Data_Science - Week4-Assignment.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Andrei Boicu** <br>
+# Data Analyst
+
+# *I am interseted in Data Science because I love looking at data and finding insights from it. Once I understand the data, it is easier to take better informed decisions.*
+
+# ### The code below adds two given numbers
+
+# In[4]:
+
+
+def add_numbers(a, b):
+    return a + b
+
+add_numbers(-10, 20)
+
+
+# **The following is a bulleted list:**
+# * Bullet 1
+# * Bullet 2
+# * Bullet 3
+# 
+# * * *
+# 
+# **The following is a numbered list:**
+# 1. Number 1
+# 2. Number 2
+# 3. Number 3
+# 
+# * * *
+# 
+# **Below is a Table:**
+# 
+# | Certification Name | Provider | Course | Year |
+# | :- | :-: | -: | -: |
+# | ***Data Science Professional Certificate*** | **IBM** | *Tools for Data Science* | 2022
+# 
+# * * *
+# 
+# **Below is a Hyperlink:** <br>
+# <a href="https://www.coursera.org/professional-certificates/ibm-data-science" target="_blank">IBM Data Science Professional Certificate</a>.
+# 
+# * * *
+# 
+# **Below is an image:**
+# 
+# ![IBM logo](https://www.ibm.com/brand/experience-guides/developer/8f4e3cc2b5d52354a6d43c8edba1e3c9/02_8-bar-reverse.svg)
+# 
+# * * *
+# 
+# `Highlighted text!`
+# 
+# * * *
+# 
+# > This is blockquoted text.
+# 
+# * * *
+# 
+# ~~This is strikethrough text.~~
+# 
+# * * *
